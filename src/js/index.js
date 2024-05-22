21019e8343128430dad594537a63730f503fc549
Luiz0032 <165849917+Luiz0032@users.noreply.github.com>
2024-05-22 19:45:54 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    mostrarMaisProjetos();
+    EsconderBotao();
+});
+
+function EsconderBotao() {
+    botaoMostrarProjetos.classList.add("remover");
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
